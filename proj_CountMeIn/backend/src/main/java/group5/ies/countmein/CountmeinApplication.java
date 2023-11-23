32b083756d35be4136228b759fa579293c9885d5
LeikRad <oreouser@gmail.com>
2023-11-23 15:33:18 +0000

    Scaffold frontend and backend

---
+++ proj_CountMeIn/backend/src/main/java/group5/ies/countmein/CountmeinApplication.java
@@ -0,0 +1,13 @@
+package group5.ies.countmein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountmeinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountmeinApplication.class, args);
+	}
+
+}
---
+++ proj_CountMeIn/backend/src/test/java/group5/ies/countmein/CountmeinApplicationTests.java
@@ -0,0 +1,13 @@
+package group5.ies.countmein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountmeinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
